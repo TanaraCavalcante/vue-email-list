92ef5230635a1fa80491e6b57582af0e9d3384dc
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-09-11 17:01:53 -0400

    talking with a backend...

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const (createApp) = Vue;
+/*const (createApp) = Vue;
 
 createApp({
     data(){
         return {
-            
+
         }
     }
-}).mount('#app')
+}).mount('#app')*/
+
+axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then(function (element){
+        console.log(element)
+    }
+);
